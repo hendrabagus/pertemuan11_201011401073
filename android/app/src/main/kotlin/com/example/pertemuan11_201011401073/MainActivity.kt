cb4fadfd9e1c5ca5586041ab7ba6d8b26ed86f3b
hendrabagus <hendrabagussetiawanto25@gmail.com>
2023-06-23 15:03:03 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/pertemuan11_201011401073/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan11_201011401073
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> dataList = [];
+
+  Future<void> fetchData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/albums');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      dataList = jsonDecode(response.body);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HTTP Request'),
+      ),
+      body: ListView.builder(
+        itemCount: dataList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(dataList[index]['title']),
+          );
+        },
+      ),
+    );
+  }
+}
